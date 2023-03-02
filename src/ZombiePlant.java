54ebd0f774e5792d7d4346c57bc04fd52a463892
Alvin Ng <alvinng@Alvin-Ngs-MacBook-Pro.local>
2023-03-02 09:34:41 +0800

    bich

---
+++ src/ZombiePlant.java
@@ -0,0 +1,29 @@
+public class ZombiePlant {
+    private int maxPotency;
+    private int treatmentsNeeded;
+
+    public ZombiePlant(int maxPotency, int treatmentsNeeded) {
+        this.maxPotency = maxPotency;
+        this.treatmentsNeeded = treatmentsNeeded;
+    }
+
+    public int getPotencyRequired() {
+        return maxPotency;
+    }
+
+    public int treatmentsNeeded() {
+        return treatmentsNeeded;
+    }
+
+    public boolean isDangerous() {
+        return (treatmentsNeeded > 0);
+    }
+
+    public void treat(int potency) {
+        if(potency <= maxPotency && potency > 0 && treatmentsNeeded > 0) {
+            treatmentsNeeded--;
+        } else if(potency > maxPotency){
+            treatmentsNeeded++;
+        }
+    }
+}
